0f20e447398a68fc839f501d58ea291e4e31a2af
BrianCobus <cobusbr2@msu.edu>
2016-02-11 18:42:32 -0500

    Update Model.php

--- Model.php
+++ Model.php
@@ -8,6 +8,42 @@
  */
 class Model
 {
-fgbbd gf
-hjg
+    /**
+     * Constructor
+     * @param $seed Random number seed
+     */
+    public function __construct($seed = null)
+    {
+
+    }
+
+    /**
+     * Create game of desired size
+     * @param $size Game size
+     */
+    public function createGame($size = 10)
+    {
+        $this->size = $size;
+    }
+
+    /**
+     * Get size
+     * @return $size Game size
+     */
+    public function getSize()
+    {
+        return $this->size;
+    }
+
+    /**
+     * Keep track of current player turn
+     * @return $size Game size
+     */
+    public function curTurn()
+    {
+
+    }
+
+
+    private $size = 0;    // Game size
 }
