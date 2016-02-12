01b86c611669d7519068ca15df0f023b097c89e9
sschmitz705 <sschmitz705@aim.com>
2016-02-12 16:21:15 -0500

    Update and rename format.ini.php to format.inc.php

--- format.inc.php
+++ format.inc.php
@@ -2,7 +2,7 @@
 function present_header($title) {
     $html = "<header>";
     $html .= "<nav><p><a href=\"welcome.php\">New Game</a> ";
-    $html .= "<a href=\"game.php\">Game</a> ";
+    $html .= "<a href=\"gameboard.php\">Game</a> ";
     $html .= "<a href=\"instructions.php\">Instructions</a></p></nav>";
     $html .= "<h1>$title</h1>";
     $html .= "</header>";
