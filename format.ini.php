60a01744912efde97308bcb742bba7f08f50ffd9
sschmitz705 <sschmitz705@aim.com>
2016-02-12 16:20:29 -0500

    Create format.ini.php

---
+++ format.ini.php
@@ -0,0 +1,11 @@
+<?php
+function present_header($title) {
+    $html = "<header>";
+    $html .= "<nav><p><a href=\"welcome.php\">New Game</a> ";
+    $html .= "<a href=\"game.php\">Game</a> ";
+    $html .= "<a href=\"instructions.php\">Instructions</a></p></nav>";
+    $html .= "<h1>$title</h1>";
+    $html .= "</header>";
+
+    return $html;
+}?>
