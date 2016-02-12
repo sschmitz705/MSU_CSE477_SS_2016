7f10c9fdfe9270c25d3e19827a757f875b8947c7
sschmitz705 <sschmitz705@aim.com>
2016-02-12 15:41:05 -0500

    Update View.php

--- lib/game/View.php
+++ lib/game/View.php
@@ -1,28 +1,23 @@
 <?php
 
-namespace game;
+namespace model;
 
 
-class gameView{
+class View{
     private $model;
     public function __construct(Model $model) {
         $this->model = $model;
     }
-    public function presentRoom($ndx) {
-        $room = $this->wumpus->getCurrent()->getNeighbors()[$ndx];
-        $roomnum = $room->getNum();
-        $roomndx = $room->getNdx();
-        $roomurl = "game-post.php?m=$roomndx";
-        $shooturl = "game-post.php?s=$roomndx";
-
-        $html = <<<HTML
-<div class="room">
-  <figure><img src="cave2.jpg" width="180" height="135" alt=""/></figure>
-  <p><a href="$roomurl">$roomnum</a></p>
-<p><a href="$shooturl">Shoot Arrow</a></p>
-</div>
-HTML;
-
+    public function parts($pl) {
+        return $html;
+    }
+    public function buttons() {
+        return $html;
+    }
+    public function player($pl) {
+        return $html;
+    }
+    public function model($pl) {
         return $html;
     }
 }
