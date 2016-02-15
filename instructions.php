40f1f456a63c81563d459776cdf13c1922b07d66
BrianCobus <cobusbr2@msu.edu>
2016-02-15 02:21:50 -0500

    Create instructions.php

---
+++ instructions.php
@@ -0,0 +1,30 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html>
+<head lang="en">
+    <meta charset="utf-8">
+    <title>Welcome to Steampunked</title>
+    <link href="project1.css" type="text/css" rel="stylesheet" />
+</head>
+<body>
+
+<div class="main">
+    <div class="image">
+        <figure><img src="images/title.png" width="500" height="100" alt="Title"/>
+        </figure>
+        <figure><img src="images/steamsplash2.png" width="400" height="400" alt="Game Board"/>
+        </figure>
+    </div>
+
+    <div class="rules">
+        <h1>INSTRUCTIONS</h1>
+      <p>Have Fun!</p>
+    </div>
+    <p><a href="gameboard.php">Return to Game</a></p>
+    <p><a href="gameboard.php">New Game</a></p>
+</div>
+
+</body>
+</html>
