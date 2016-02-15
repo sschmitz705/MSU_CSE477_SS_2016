8e243ac7323b00cf97ae759fc9c17e0a7cf3c775
BrianCobus <cobusbr2@msu.edu>
2016-02-15 03:17:22 -0500

    Update format.inc.php

--- format.inc.php
+++ format.inc.php
@@ -4,7 +4,7 @@ function present_header($title) {
     $html .= "<nav><p><a href=\"welcome.php\">New Game</a> ";
     $html .= "<a href=\"gameboard.php\">Game</a> ";
     $html .= "<a href=\"instructions.php\">Instructions</a></p></nav>";
-    $html .= "<h1>$title</h1>";
+    $html .= "<figure><img src=\"images/title.png\" width=\"500\" height=\"100\" alt=\"Title\"/></figure>";
     $html .= "</header>";
 
     return $html;
