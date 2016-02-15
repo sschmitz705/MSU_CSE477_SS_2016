6ec41fa96d69162bdf423b769d11d535c7db03ce
BrianCobus <cobusbr2@msu.edu>
2016-02-15 03:25:48 -0500

    Update welcome.php

--- welcome.php
+++ welcome.php
@@ -6,20 +6,21 @@ require 'format.inc.php';
 <head>
     <meta charset="UTF-8">
     <title>Game</title>
-    <link href="game.css" type="text/css" rel="stylesheet" />
+    <link href="project1.css" type="text/css" rel="stylesheet" />
 </head>
 <body>
+
 <?php echo present_header("STEAMPUNKED"); ?>
+
 <article>
-    <section id="cave">
-        <figure>
-            //<img src="cave-evil-cat.png" width="600" height = "325" alt="cave"/>
+    <section id="image">
+        <figure><img src="images/steamsplash2.png" width="400" height="400" alt="Game Board"/>
         </figure>
     </section>
     <section id="room text" style="padding-bottom: 20px;">
-        <h1>Welcome to <its font-style="italic">Stalking the Wampus</its></h1>
+        <h1>Welcome to <its font-style="italic">STEAMPUNKED</its></h1>
         <br>
-        <h1><a href="game.php">Start Game</a></h1>
+        <h1><a href="gameboard.php">Start Game</a></h1>
         <h1><a href="instructions.php">Instructions</a></h1>
     </section>
 </article>
