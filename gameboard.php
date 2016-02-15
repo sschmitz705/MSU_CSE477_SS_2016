d692cc3b3ab190474ca5a63c002d7c030e46eab9
BrianCobus <cobusbr2@msu.edu>
2016-02-15 02:30:24 -0500

    Create gameboard.php

---
+++ gameboard.php
@@ -0,0 +1,28 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head lang="en">
+    <meta charset="utf-8">
+    <title>Steampunked</title>
+    <link href="project1.css" type="text/css" rel="stylesheet" />
+</head>
+<body>
+
+<div class="image">
+    <figure><img src="images/title.png" width="500" height="100" alt="Title"/>
+    </figure>
+</div>
+
+<form>
+   <div class="game">
+      <div class="row">
+         <div class="cell"></div><div class="cell"></div><div class="cell"></div>
+      </div>
+      <div class="row">
+         <div class="cell"></div><div class="cell"></div><div class="cell"></div>
+      </div>
+   </div>
+</form>
